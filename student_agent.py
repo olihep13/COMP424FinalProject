1e535dc3f84f45f51dce44bc9188b979b098a8e7
Olivier Hepler <32748124+olihep13@users.noreply.github.com>
2023-12-03 16:14:33 -0500

    Update student_agent.py

--- student_agent.py
+++ student_agent.py
@@ -7,7 +7,6 @@ from copy import deepcopy
 import time
 import pdb
 
-
 @register_agent("student_agent")
 class StudentAgent(Agent):
     """
@@ -25,107 +24,179 @@ class StudentAgent(Agent):
             "l": 3,
         }
 
+    def minimax(self, chess_board, root, my_pos, adv_pos, max_step, depth, maximizing_player, alpha, beta):
 
-    def minimax(self, chess_board, root, my_pos, adv_pos, max_step, depth, maximizing_player):
-
-        score, gameOver = self.evaluate(root.board, my_pos, adv_pos, maximizing_player)
-        root.nextStep = root.pos, 1
-
-        if depth == 0 or gameOver:
+        score, gameOver = self.evaluate(root.board, my_pos, adv_pos, maximizing_player, max_step)
+        if gameOver or depth == 0:
             return score
-
-        mapVisited = {}
-        root.children = self.oneStepAway(chess_board, my_pos, adv_pos, max_step, mapVisited, maximizing_player)
+        map_visited = {}
+        root.children = self.oneStepAway(chess_board, my_pos, adv_pos, max_step, map_visited)
 
         if maximizing_player:
-            maxEval = -5
-            for i in range(len(root.children)):
-                if root.children[i] != []:
-                    eval = self.minimax(root.children[i].board, root.children[i], root.children[i].advPos, root.children[i].pos, max_step, depth-1, False)
-                    if maxEval < eval:
-                        maxEval = eval
-                        root.nextStep = root.children[i].pos, root.children[i].direction
-                return maxEval
+            max_eval = None
+            for i in root.children:
+                cur_eval = self.minimax(i.board, i, i.advPos, i.pos, max_step, depth - 1, False, alpha, beta)
+                if max_eval == None:
+                    max_eval = cur_eval
+                    root.nextStep = i.pos, i.direction
+                elif max_eval < cur_eval:
+                    max_eval = cur_eval
+                    root.nextStep = i.pos, i.direction
+                if alpha == None:
+                    alpha = max_eval
+                else:
+                    alpha = max(alpha, max_eval)
+                if beta != None and alpha != None and beta <= alpha:
+                    break
+            return max_eval
         else:
-            minEval = 5
-            for i in range(len(root.children)):
-                if root.children[i] != []:
-                    eval = self.minimax(root.children[i].board, root.children[i], root.children[i].advPos, root.children[i].pos, max_step, depth-1, True)
-                    if minEval > eval:
-                        minEval = eval
-                        root.nextStep = root.children[i].pos, root.children[i].direction
-                return minEval
-        return 0
+            min_eval = None
+            for i in root.children:
+                cur_eval = self.minimax(i.board, i, i.advPos, i.pos, max_step, depth - 1, True, alpha, beta)
+                if min_eval == None:
+                    min_eval = cur_eval
+                    root.nextStep = i.pos, i.direction
+                elif min_eval > cur_eval:
+                    min_eval = cur_eval
+                    root.nextStep = i.pos, i.direction
+                if beta == None:
+                    beta = min_eval
+                else:
+                    beta = min(beta, min_eval)
+                if beta != None and alpha != None and beta <= alpha:
+                    break
+            return min_eval
 
-    def oneStepAway(self, chess_board, my_pos, adv_pos, max_step, map, maxizingPlayer):
-
-        score, gameOver = self.evaluate(chess_board, my_pos, adv_pos, maxizingPlayer)
-
-        if max_step == 0 or gameOver:
+    def oneStepAway(self, chess_board, my_pos, adv_pos, max_step, my_map,) -> list:
+        # should we return an empty list or a -1 -1 node
+        # instead of returning empty lists could we check max step beforehand
+        if max_step == 0:
             return []
 
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
         list1 = []
         r, c = my_pos
-
-        # Build a list of the moves we can make
+        # Checks if we can move there
         allowed_dirs = [d
                         for d in range(0, 4)  # 4 moves possible
                         if not chess_board[r, c, d] and  # chess_board True means wall
                         not adv_pos == (r + moves[d][0], c + moves[d][1])]  # cannot move through Adversary
-
+        # do we need this or does our endgame function take care of this already
+        # if we move there and we can't move anywhere else we should remove this node from our list
+        # but we've already appended it before the recursion, maybe we just return -1,-1
         if len(allowed_dirs) == 0:
-            # If no possible move, we must be enclosed by our Adversary
+            # this node should never get to an evaluate or check endgame function
             return list1
+            # even though we call evaluate or check endgame in one step away
+            # OneStepAway is only called on nodes that take value mypos, not every node part of our big list
+            # so we will never evaluate -1,-1 from this function
+            # minimax has qualifying statements before each eval function
 
+
+        # for moves, we can make
         for i in range(len(allowed_dirs)):
+            # iterate through each move
             dir = allowed_dirs[i]
+            # row and column
             m_r, m_c = moves[dir]
+            # add the difference to your position
             my_pos = (r + m_r, c + m_c)
-
-            allowed_barriers = [j for j in range(0, 4) if not chess_board[r + m_r, c + m_c, j]]
+            # make a list of walls for each move
+            allowed_barriers = [j for j in range(0, 4)
+                                if not chess_board[r + m_r, c + m_c, j]]
 
             if len(allowed_barriers) == 0:
-                # If no possible move, we must be enclosed by our Adversary
-                return list1
-
+                # if we can't put up any walls then the position is going to be avoided
+                continue
+            # for each wall
             for x in range(len(allowed_barriers)):
-                if map.get((my_pos, allowed_barriers[x])) is None:
-                    map[(my_pos, allowed_barriers[x])] = 1
+                # if my position is not in the map
+                if my_map.get((my_pos, allowed_barriers[x])) is None:
+                    my_map[(my_pos, allowed_barriers[x])] = 1
                     node = Tree()
+                    # I don't think we need to deepcopy the chess_board until we iterate through the nodes
                     node.board = deepcopy(chess_board)
                     node.board[r, c, allowed_barriers[x]] = True
                     node.pos = my_pos
                     node.direction = allowed_barriers[x]
                     node.advPos = adv_pos
                     list1.append(node)
-                    list1.append(self.oneStepAway(node.board, node.pos, node.advPos, max_step-1, map, maxizingPlayer))
+                    # combine lists we could
+                    list1 = list1 + self.oneStepAway(chess_board, node.pos, node.advPos, max_step - 1, my_map)
+            # should not return a list that will be empty, should append all the elements to a list and make sure
+            # it is not 2,3 or 5D we might not want to return a list here but after the loop has gone through
+            # everything
+        return list1
 
-            return list1
-
-    def evaluate(self, chess_board, my_pos, adv_pos, maximizingPlayer):
+    def evaluate(self, chess_board, my_pos, adv_pos,  maximizing_player, max_step):
 
         # if minimizing:
-            # if adv wins return high num, if you win return low num
+        # if adv wins return high num, if you win return low num
         # if maximizing:
-            # if adv wins return low num, if you win return high num
+        # if adv wins return low num, if you win return high num
 
         # never build the 4th wall when surrounded by 3 walls, always try to exit
         # maximize the available moves to you
 
-        #
-
         # here we are checking to see if the game is over for either player and returning -1 or 1
         # based on whether we are checking for maximizing player and whether adversary or you loose
 
-        (gameOver, myPlayerScore, advPlayerScore) = self.check_endgame(chess_board, my_pos, adv_pos)
+        # I replaced onestepaway here with blocks available cause it's much faster since it doesn't deepcopy
+        # and has lower number of squares
 
-        if maximizingPlayer:
+        #mcts
+
+        advPlayerScore = self.blocks_available(chess_board, adv_pos, my_pos, max_step, {})
+        myPlayerScore = self.blocks_available(chess_board, my_pos, adv_pos, max_step, {})
+
+        if advPlayerScore == 0 or myPlayerScore == 0:
+            gameOver = True
+        else:
+            gameOver = False
+
+        if maximizing_player:
             score = (myPlayerScore - advPlayerScore) / (len(chess_board[0]))
+            # + if maximizing player
         else:
             score = (advPlayerScore - myPlayerScore) / (len(chess_board[0]))
+            # - if minimizing player
 
-        return (score, gameOver) # or divide by sum of two player scores, not sure
+        return score, gameOver  # or divide by sum of two player scores, not sure
+
+    def random_move(self, board, my_p, adv_p, max_s):
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        steps = np.random.randint(0, max_s + 1)
+
+        # Pick steps random but allowable moves
+        for _ in range(steps):
+            r, c = my_p
+
+            # Build a list of the moves we can make
+            allowed_dirs = [ d
+                for d in range(0,4)                           # 4 moves possible
+                if not board[r,c,d] and                 # if not a wall and not our opponents position chess_board True means wall
+                not adv_p == (r+moves[d][0],c+moves[d][1])] # cannot move through Adversary
+
+            if len(allowed_dirs)==0:
+                # If no possible move, we must be enclosed by our Adversary
+                break
+
+            random_dir = allowed_dirs[np.random.randint(0, len(allowed_dirs))]
+
+            # This is how to update a row,col by the entries in moves
+            # to be consistent with game logic
+            m_r, m_c = moves[random_dir]
+            my_p = (r + m_r, c + m_c)
+
+        # Final portion, pick where to put our new barrier, at random
+        r, c = my_p
+        # Possibilities, any direction such that chess_board is False
+        allowed_barriers=[i for i in range(0,4) if not board[r,c,i]]
+        # Sanity check, no way to be fully enclosed in a square, else game already ended
+        assert len(allowed_barriers) >= 1
+        dir = allowed_barriers[np.random.randint(0, len(allowed_barriers))]
+
+        return my_p, dir
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -143,7 +214,9 @@ class StudentAgent(Agent):
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
 
-        # Some simple code to help you with timing. Consider checking 
+
+
+        # Some simple code to help you with timing. Consider checking
         # time_taken during your search and breaking with the best answer
         # so far when it nears 2 seconds.
         start_time = time.time()
@@ -153,76 +226,53 @@ class StudentAgent(Agent):
         root.board = chess_board
         root.pos = my_pos
         root.advPos = adv_pos
-        root.nextStep = my_pos, 1
-        self.minimax(root.board, root, root.pos, root.advPos, max_step, 10, True)
-        # minimax will be going down our tree and using evaluation to find the next best node to move into
+        root.nextStep = self.random_move(chess_board, my_pos, adv_pos, max_step)
+
+        max_eval = self.minimax(root.board, root, root.pos, root.advPos, max_step, 2, True, None, None)
 
         time_taken = time.time() - start_time
 
-        return root.nextStep #sometimes returns none
+        return root.nextStep
 
-    def check_endgame(self, chess_board, my_pos, adv_pos):
-        """
-        Check if the game ends and compute the current score of the agents.
+    def blocks_available(self, chess_board, my_pos, adv_pos, max_steps, my_map):
+        # THE CHECKENDGAME TAKES TOO LONG TO BE RUN
+        # score, gameOver = self.evaluate(chess_board, my_pos, adv_pos, maximizing_player)
+        # if gameOver:
+        #   return[]
 
-        Returns
-        -------
-        is_endgame : bool
-            Whether the game ends.
-        player_1_score : int
-            The score of player 1.
-        player_2_score : int
-            The score of player 2.
-        """
-        board_size = len(chess_board[0])
+        #return a value instead
+
+        if max_steps == 0:
+            return 0
+
         moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        sum = 0
+        r, c = my_pos
+        # Checks if we can move there
+        allowed_dirs = [d
+                        for d in range(0, 4)  # 4 moves possible
+                        if not chess_board[r, c, d] and  # chess_board True means wall
+                        not adv_pos == (r + moves[d][0], c + moves[d][1])]  # cannot move through Adversary
+        if len(allowed_dirs) == 0:
+            return 0
+        for i in range(len(allowed_dirs)):
+            # iterate through each move
+            dir = allowed_dirs[i]
+            # row and column
+            m_r, m_c = moves[dir]
+            # add the difference to your position
+            my_pos = (r + m_r, c + m_c)
+            # make a list of walls for each move
+            allowed_barriers = [j for j in range(0, 4)
+                                if not chess_board[r + m_r, c + m_c, j]]
 
-        # Union-Find
-        father = dict()
-        for r in range(board_size):
-            for c in range(board_size):
-                father[(r, c)] = (r, c)
-
-        def find(pos):
-            if father[pos] != pos:
-                father[pos] = find(father[pos])
-            return father[pos]
-
-        def union(pos1, pos2):
-            father[pos1] = pos2
-
-        for r in range(board_size):
-            for c in range(board_size):
-                for dir, move in enumerate(
-                    moves[1:3]
-                ):  # Only check down and right
-                    if chess_board[r, c, dir + 1]:
-                        continue
-                    pos_a = find((r, c))
-                    pos_b = find((r + move[0], c + move[1]))
-                    if pos_a != pos_b:
-                        union(pos_a, pos_b)
-
-        for r in range(board_size):
-            for c in range(board_size):
-                find((r, c))
-        p0_r = find(tuple(my_pos))
-        p1_r = find(tuple(adv_pos))
-        p0_score = list(father.values()).count(p0_r)
-        p1_score = list(father.values()).count(p1_r)
-        if p0_r == p1_r:
-            return (False, p0_score, p1_score)
-        player_win = None
-        win_blocks = -1
-        if p0_score > p1_score:
-            player_win = 0
-            win_blocks = p0_score
-        elif p0_score < p1_score:
-            player_win = 1
-            win_blocks = p1_score
-        else:
-            player_win = -1  # Tie
-        return (True, p0_score, p1_score)
+            if my_map.get(my_pos) is None:
+                my_map[my_pos] = 1
+                sum = sum + len(allowed_barriers) + self.blocks_available(chess_board, my_pos, adv_pos, max_steps - 1, my_map)
+            # should not return a list that will be empty, should append all the elements to a list and make sure
+            # it is not 2,3 or 5D we might not want to return a list here but after the loop has gone through
+            # everything
+        return sum
 
 class Tree:
     def __init__(self):
@@ -231,4 +281,5 @@ class Tree:
         self.direction = None
         self.advPos = []
         self.children = []
-        self.nextStep = None
+        self.nextStep = []
+        self.numMoves = 0
